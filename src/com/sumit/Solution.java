79a9b989a4176a987c0eb833307eb0a2c2c220db
yaminikrishna07 <yaminikrishna3127@gmail.com>
2021-03-11 01:17:26 -0500

    Search In BST by Sumit Saurabh

---
+++ src/com/sumit/Solution.java
@@ -0,0 +1,51 @@
+package com.sumit;
+
+public class Solution {
+	
+	//structure of tree node
+	public class TreeNode{
+				
+				int val;
+				TreeNode left;
+				TreeNode right;
+				TreeNode(){}
+				TreeNode(int val)
+				{
+					this.val=val;
+				}
+				
+				TreeNode(int val, TreeNode left, TreeNode right)
+				{
+					this.val=val;
+					this.left=left;
+					this.right =right;
+				}
+	}
+	
+   public TreeNode searchBST(TreeNode root, int val) {
+        
+        if(root==null)
+        {
+            return null;
+        }
+        
+        if(root.val==val)
+        {
+            return root;
+        }
+        
+        if(val<root.val)
+        {
+            return searchBST(root.left,val);
+        }
+        
+        if(val>root.val)
+        {
+            return searchBST(root.right,val);
+        }
+        
+        return null;
+    }
+    
+
+}
